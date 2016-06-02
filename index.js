812025f11fe1125ca34fc25d4ef017c2c495501a
Dan Phillips <dphillips@mic.com>
2016-06-02 18:23:32 -0400

    appease the linter

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 module.exports = function (obj, blacklist) {
-  function replacer(key, val) {
+  function replacer (key, val) {
     if (isValidForBuild(key, val)) {
       return val;
     } else {
       return undefined;
     }
-  };
+  }
 
-  function isValidForBuild(key, val) {
+  function isValidForBuild (key, val) {
     if (isBlacklisted(key) || !isReferenceToGitRepo(key, val)) {
-        return false;
+      return false;
     }
 
     return true;
-  };
+  }
 
-  function isBlacklisted(key) {
+  function isBlacklisted (key) {
     return blacklist.has(key);
-  };
+  }
 
-  function isReferenceToGitRepo(key, val) {
+  function isReferenceToGitRepo (key, val) {
     var isReference = (key === 'resolved' || key === 'from');
 
     if (isReference && !isGitRepo(val)) {
@@ -27,11 +27,11 @@ module.exports = function (obj, blacklist) {
     }
 
     return true;
-  };
+  }
 
-  function isGitRepo(rep) {
-      return /^git/.test(rep);
-  };
+  function isGitRepo (rep) {
+    return /^git/.test(rep);
+  }
 
   return JSON.stringify(obj, replacer, 2);
 };
--- test/test.js
+++ test/test.js
@@ -1,35 +1,34 @@
 var test = require('tape');
-var shankwrap = require('../index')
+var shankwrap = require('../index');
 
-const TEST_OBJECT =
-{
-  "name": "black pearl",
-  "version": "1.0",
-  "dependencies": {
-    "jack-sparrow": {
-      "version": "1.6.5",
-      "from": "jack-sparrow@>=1.0.0 <2.0.0",
-      "resolved": "https://registry.npmjs.org/jack-sparrow/-/jack-sparrow-1.6.5.tgz",
-      "dependencies": {
-        "async": {
-          "version": "2.0.0-rc.5",
-          "from": "async@>=2.0.0-rc.3 <3.0.0",
-          "resolved": "https://registry.npmjs.org/async/-/async-2.0.0-rc.5.tgz"
+const TEST_OBJECT = {
+  'name': 'black pearl',
+  'version': '1.0',
+  'dependencies': {
+    'jack-sparrow': {
+      'version': '1.6.5',
+      'from': 'jack-sparrow@>=1.0.0 <2.0.0',
+      'resolved': 'https://registry.npmjs.org/jack-sparrow/-/jack-sparrow-1.6.5.tgz',
+      'dependencies': {
+        'async': {
+          'version': '2.0.0-rc.5',
+          'from': 'async@>=2.0.0-rc.3 <3.0.0',
+          'resolved': 'https://registry.npmjs.org/async/-/async-2.0.0-rc.5.tgz'
         },
-        "lodash": {
-          "version": "4.13.1",
-          "from": "lodash@>=4.8.0 <5.0.0",
-          "resolved": "https://registry.npmjs.org/lodash/-/lodash-4.13.1.tgz"
+        'lodash': {
+          'version': '4.13.1',
+          'from': 'lodash@>=4.8.0 <5.0.0',
+          'resolved': 'https://registry.npmjs.org/lodash/-/lodash-4.13.1.tgz'
         },
-        "foreground-child": {
-          "version": "1.3.0",
-          "from": "git+https://github.com/bcoe/foreground-child.git#win-spawn",
-          "resolved": "git+https://github.com/bcoe/foreground-child.git#ad7ba1d4a84e8b199cbfb5d30e01e586390978cc",
-          "dependencies": {
-            "win-spawn": {
-              "version": "2.0.0",
-              "from": "win-spawn@>=2.0.0 <3.0.0",
-              "resolved": "https://registry.npmjs.org/win-spawn/-/win-spawn-2.0.0.tgz"
+        'foreground-child': {
+          'version': '1.3.0',
+          'from': 'git+https://github.com/bcoe/foreground-child.git#win-spawn',
+          'resolved': 'git+https://github.com/bcoe/foreground-child.git#ad7ba1d4a84e8b199cbfb5d30e01e586390978cc',
+          'dependencies': {
+            'win-spawn': {
+              'version': '2.0.0',
+              'from': 'win-spawn@>=2.0.0 <3.0.0',
+              'resolved': 'https://registry.npmjs.org/win-spawn/-/win-spawn-2.0.0.tgz'
             }
           }
         }
@@ -39,7 +38,7 @@ const TEST_OBJECT =
 };
 
 test('it does not remove non-dependency related keys', function (t) {
-  var obj = { "don't hurt me": "i'm friendly" };
+  var obj = { 'don\'t hurt me': 'i\'m friendly' };
 
   t.equal(JSON.stringify(obj, {}, 2), shankwrap(obj, new Set()));
 
